22e8610baa63394722ca98fca0cef5964728b88d
Rahulsharma522 <sharma.sharma.rahul46@gmail.com>
2021-09-17 10:30:50 +0530

    level 2& 3

--- block-BJaabv/code/index.js
+++ block-BJaabv/code/index.js
@@ -8,9 +8,11 @@
   * [ ] Add an additional argument to the function that takes the conversion rate of human to dog years.
 */
 
-function calculateDogAge() {
-  // Your code goes here
+function calculateDogAge(age) {
+  const puppyAge = 7;
+  return puppyAge * age;
 }
+calculateDogAge();
 
 /*
 2. 🎖Write a function named calculateMoviesToWatch that:
@@ -19,9 +21,12 @@ function calculateDogAge() {
   * [ ] Return the total number of movie you will watch.
 */
 
-function calculateMoviesToWatch() {
-  // Your code goes here
+function calculateMoviesToWatch(age, moviesInWeek) {
+  const weeksInMonth = 4;
+  const monthInYears = 12;
+  return age * moviesInWeek * weeksInMonth * monthInYears;
 }
+calculateMoviesToWatch();
 
 /*
 3. 🎖Create a function called celsiusToFahrenheit:
@@ -29,9 +34,13 @@ function calculateMoviesToWatch() {
   * [ ] Convert it to fahrenheit and return "NN°F is NN°C"
 */
 
-function celsiusToFahrenheit() {
-  // Your code goes here
+function celsiusToFahrenheit(temperatureInCelsius) {
+  const div1 = 9;
+  const div2 = 5;
+  const add = 32;
+  return (temperatureInCelsius * div1) / div2 + add;
 }
+celsiusToFahrenheit();
 
 /*
 4. 🎖Create a function called fahrenheitToCelsius:
@@ -39,20 +48,26 @@ function celsiusToFahrenheit() {
   * [ ] Convert it to celsius and return "NN°F is NN°C"
 */
 
-function celsiusToFahrenheit() {
-  // Your code goes here
+function fahrenheitToCelsius(temperatureInFahrenheit) {
+  const div1 = 9;
+  const div2 = 5;
+  const sub = 32;
+  return temperatureInFahrenheit - (sub * div1) / div2;
 }
+fahrenheitToCelsius();
 
 /*
 5. 🎖 Write a function pow(n, x) that returns x in power n.
-
   * [ ] The function accepts two parameter x and n.
   * [ ] Return x times n
   * [ ] If the value of n is below 0 return "The number below 1 is not allowed"
 */
 
-function pow() {
-  // Your code goes here
+function pow(n, x) {
+  if (n < 0) {
+    return `The number below 1 is not allowed`;
+  }
+  return n ** x;
 }
 
 // Test
@@ -70,17 +85,20 @@ function sumOrProductOfN() {
   // Your code goes here
 }
 
-sumOrProductOfN(4, 'sum'); // 10
-sumOrProductOfN(4, 'product'); // 24
-sumOrProductOfN(4, 'hello'); // alert "Not a valid Input"
+sumOrProductOfN(4, "sum"); // 10
+sumOrProductOfN(4, "product"); // 24
+sumOrProductOfN(4, "hello"); // alert "Not a valid Input"
 
 /*
 6. 🎖Write a program that accepts a number n using argument and return the sum of the numbers 1 to n
 */
 
-function sumOfN() {
-  // Your code goes here
+function sumOfN(n) {
+  let sum = 1;
+  for (let i = 1; i <= n; i++) sum += i;
+  return sum;
 }
+sumOfN();
 
 /*
 7. 🎖Modify the previous program such that only multiples of 5 or 7 are considered in the sum, e.g. n = 20 (5,7,10,14,15,20) 71
@@ -92,8 +110,12 @@ function sumOfN() {
 8. 🎖Write a function `min` that takes two arguments and returns their minimum.
 */
 
-function min() {
-  // Your code goes here
+function min(a, b) {
+  if (a > b) {
+    return b;
+  } else if (a < b) {
+    return a;
+  }
 }
 
 min(0, 10);
@@ -103,6 +125,7 @@ min(0, -10);
 9. 🎖Write a JavaScript function which accepts an argument and returns the type of the value.
 */
 
-function typeCheck() {
-  // Your code goes here
+function typeCheck(n) {
+  return typeof n;
 }
+typeof "Rahul";
